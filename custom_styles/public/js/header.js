720bbfe13831e7c6d420ccca96f84a96a95ba16c
Dave Starke <dave@starkapps.com>
2018-04-01 08:16:31 +0000

    page style

--- custom_styles/public/js/header.js
+++ custom_styles/public/js/header.js
@@ -3,36 +3,46 @@ $(document).ready(function() {
         
     // Clean up header
     $(".navbar-home").html("Home");
-    // Add clock
-    $('.navbar-right').append('<li id="header-clock" class="hidden-xs hidden-sm"></div>');
+
     
     // hack to make all pages 12 columns except for the modules page.
     // & Set time for header.
-    setInterval(function(){
+    
+    function isMobileDevice() {
+        return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+    };
+    
+    if !(isMobileDevice()) {
+            // Add clock
+            $('.navbar-right').append('<li id="header-clock" class="hidden-xs hidden-sm"></div>');
         
-        var timeNow = moment().format('h:mm a');
-        $('#header-clock').html(timeNow);
-        
-        var winLoc = window.location.href;
-        var leftCol = $(".layout-side-section");
-        var rightCol = $(".layout-main-section-wrapper");
-        
-        if (winLoc.indexOf("desk#modules") >= 0 ||
-            winLoc.indexOf("desk#Form/Item") >= 0) {
-            console.log("We are at the Module Screen! " + $.now());          
-            
-            rightCol.removeClass("col-md-12");
-            rightCol.addClass("col-md-10");
-            rightCol.css("padding-left","0");
-            leftCol.show();
-                        
-            console.log(winLoc + + $.now());
-        } else {
-            leftCol.hide();
-            rightCol.removeClass("col-md-10");
-            rightCol.css("padding-left","15px");
-            rightCol.addClass("col-md-12");
-        }
+            setInterval(function(){
                 
-    }, 500); 
+                var timeNow = moment().format('h:mm a');
+                $('#header-clock').html(timeNow);
+                
+                var winLoc = window.location.href;
+                var leftCol = $(".layout-side-section");
+                var rightCol = $(".layout-main-section-wrapper");
+                
+                if (winLoc.indexOf("desk#modules") >= 0 ||
+                    winLoc.indexOf("desk#Form/Item") >= 0) {
+                    console.log("We are at the Module Screen! " + $.now());          
+                    
+                    rightCol.removeClass("col-md-12");
+                    rightCol.addClass("col-md-10");
+                    rightCol.css("padding-left","0");
+                    leftCol.show();
+                                
+                    console.log(winLoc + + $.now());
+                } else {
+                    leftCol.hide();
+                    rightCol.removeClass("col-md-10");
+                    rightCol.css("padding-left","15px");
+                    rightCol.addClass("col-md-12");
+                }
+                        
+            }, 500);  
+        
+        } 
 });
